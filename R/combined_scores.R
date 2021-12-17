e2db7e6be884ec315ba8e3bb8bce28d1e3093c8a
Carl <cboettig@gmail.com>
2021-12-17 01:00:16 +0000

    using new scoring reader

---
+++ R/combined_scores.R
@@ -0,0 +1,16 @@
+
+
+combined_scores <- function(theme){
+  Sys.setenv("AWS_EC2_METADATA_DISABLED"="TRUE")
+  Sys.unsetenv("AWS_ACCESS_KEY_ID")
+  Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
+  Sys.unsetenv("AWS_DEFAULT_REGION")
+  Sys.unsetenv("AWS_S3_ENDPOINT")
+
+  s <- neon4cast::score_schema()
+  s3 <- arrow::s3_bucket(bucket = "scores",
+                         endpoint_override = "data.ecoforecast.org",
+                         anonymous=TRUE)
+  ds <- arrow::open_dataset(s3, schema=s, format = "csv", skip_rows = 1)
+  ds %>% filter(theme == theme)
+}
