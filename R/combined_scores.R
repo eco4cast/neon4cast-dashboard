ed075c31a2171cb64a591945516c7991a866d525
Carl <cboettig@gmail.com>
2022-03-03 21:48:03 +0000

    fix combined scores

--- R/combined_scores.R
+++ R/combined_scores.R
@@ -1,6 +1,6 @@
 
 
-combined_scores <- function(x, collect = TRUE){
+combined_scores <- function(x = NULL, collect = TRUE){
   Sys.setenv("AWS_EC2_METADATA_DISABLED"="TRUE")
   Sys.unsetenv("AWS_ACCESS_KEY_ID")
   Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
@@ -12,9 +12,10 @@ combined_scores <- function(x, collect = TRUE){
                          endpoint_override = "data.ecoforecast.org",
                          anonymous=TRUE)
   ds <- arrow::open_dataset(s3, schema=s, format = "csv", skip_rows = 1)
-  df <- ds %>% filter(theme == x)
+  if(!is.null(x))
+    ds <- ds %>% dplyr::filter(theme == {{x}})
   if(collect) {
-   df <- df %>% collect()
+    ds <- ds %>% collect()
   }
-  df
+  ds
 }
