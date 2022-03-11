24d2da281b236ea2bc89fd0bf12beeeb25528ece
Carl <cboettig@gmail.com>
2022-03-11 00:36:08 +0000

    mean score calculation

--- R/combined_scores.R
+++ R/combined_scores.R
@@ -1,6 +1,9 @@
+library(arrow)
 library(glue)
 library(fs)
 library(magrittr)
+library(dplyr)
+library(tidyr)
 
 combined_scores <- function(theme = "", year = "", collect = TRUE,
                             endpoint =  "minio.carlboettiger.info"
@@ -18,8 +21,44 @@ combined_scores <- function(theme = "", year = "", collect = TRUE,
   files <- s3$path(fs::path_norm(path))
   ds <- arrow::open_dataset(files)
 
-  if(collect) {
+  if (collect) {
     ds <- ds %>% collect()
   }
   ds
 }
+
+
+fill_scores <- function(df, null_team = NA) {
+  df <- df %>% filter(!is.na(observed)) %>% collect()
+
+  team <- distinct(df,team)
+  if (is.na(null_team)) {
+    x <- pull(team,team)
+    null_team <- x[grepl("null", x)]
+  }
+
+  null <- df %>%
+    filter(team == null_team) %>%
+    select("theme", "target", "x","y","z", "site", "time",
+           "forecast_start_time", "crps", "logs")
+  all <- tidyr::expand_grid(null, team)
+  na_filled <- left_join(all, df,
+                         by = c("theme", "team", "target", "x","y","z",
+                                "site", "time", "forecast_start_time"),
+                         suffix = c("_null", "_team"))
+  null_filled <- na_filled %>% mutate(
+    crps = case_when(is.na(crps_team) ~ crps_null,
+                     !is.na(crps_team) ~ crps_team),
+    logs = case_when(is.na(logs_team) ~ logs_null,
+                     !is.na(logs_team) ~ logs_team)) %>%
+    select(-crps_null, -logs_null)
+
+  ## express difftimes in days, not seconds
+  null_filled %>% mutate(interval = as.numeric(interval, units="days"),
+                         horizon = as.numeric(horizon, units="days"))
+
+}
+
+
+
+
