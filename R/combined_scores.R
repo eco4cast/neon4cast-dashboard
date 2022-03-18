0b5ea89ef6d6aa54bfd39f147d3aac428c3f1eba
Carl Boettiger <cobettig@gmail.com>
2022-03-18 21:13:32 +0000

    fix helper fn again!

--- R/combined_scores.R
+++ R/combined_scores.R
@@ -5,24 +5,25 @@ library(magrittr)
 library(dplyr)
 library(tidyr)
 
-combined_scores <- function(theme = "", year = "", collect = TRUE,
-                            endpoint =  "minio.carlboettiger.info"
-                          #  endpoint = "data.ecoforecast.org"
-                              ){
+
+combined_scores <- function(theme = NA,
+                            collect = TRUE,
+                            endpoint = "minio.carlboettiger.info"){
   Sys.setenv("AWS_EC2_METADATA_DISABLED"="TRUE")
   Sys.unsetenv("AWS_ACCESS_KEY_ID")
   Sys.unsetenv("AWS_SECRET_ACCESS_KEY")
   Sys.unsetenv("AWS_DEFAULT_REGION")
   Sys.unsetenv("AWS_S3_ENDPOINT")
 
-  s3 <- arrow::s3_bucket(bucket = "scores", endpoint_override = endpoint)
-
-  path <- glue::glue("parquet/{theme}/{year}", theme = theme, year = year)
-  files <- s3$path(fs::path_norm(path))
-  ds <- arrow::open_dataset(files)
-
+  #GENERALIZATION: THIS IS A SPECIFIC ENDPOINT
+  s3 <- arrow::s3_bucket(bucket = "scores",
+                         endpoint_override = endpoint)
+  ds <- arrow::open_dataset(s3, partition=c("theme", "year"))
+  if (!is.na(theme)) {
+    ds <- dplyr::filter(ds, theme == {{theme}})
+  }
   if (collect) {
-    ds <- ds %>% collect()
+    ds <- dplyr::collect(ds)
   }
   ds
 }
