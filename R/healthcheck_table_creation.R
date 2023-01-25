976593bcfe8b18e96e3b7ac040c469041381110f
addelany <addelany@gmail.com>
2023-01-25 20:40:54 +0000

    add function for health checks table and update related code with the status.qmd file (automated processes and noaa processes). Also added times to healthchecks

--- R/healthcheck_table_creation.R
+++ R/healthcheck_table_creation.R
@@ -16,18 +16,21 @@ check_parsed <- content(health_check,as='parsed')$checks
 
 hc_names <- c()
 hc_status <- c()
+hc_latest <- c()
 
 for (i in seq.int(1,length(check_parsed))){
-  hc_names[i] <- check_parsed[[i]][[1]] # json objects 1 and 7 are the name and status, respectively
+  hc_names[i] <- check_parsed[[i]][[1]] # json objects 1,7,9 are name, status, and last ping
   hc_status[i] <- check_parsed[[i]][[7]]
+  hc_latest[i] <- check_parsed[[i]][[9]]
 }
 
-check_df <- data.frame(hc_names,hc_status)
-
+check_df <- data.frame(hc_names,hc_status,hc_latest)
+check_df$hc_latest <- as.character(lubridate::as_datetime(check_df$hc_latest))
 
 reactable(check_df,
           columns = list(hc_names = colDef(name='Process Name'),
-                         hc_status = colDef(name='Status')),
+                         hc_status = colDef(name='Status'),
+                         hc_latest = colDef(name = 'Updated (UTC)')),
           defaultPageSize = 20,
           filterable = TRUE,
           highlight = TRUE)
--- R/status_functions.R
+++ R/status_functions.R
@@ -63,3 +63,66 @@ theme_forecast_table <- function(forecast_theme){
 
  return(final_table)
 }
+
+
+## function for health check table
+
+health_check_table <- function(context){
+
+  health_check <- GET(
+    "https://healthchecks.io/api/v1/checks/",
+    accept_json(),
+    add_headers('X-Api-Key' = 'YKpvNOQWGtcGtyKu4rgC0Z7_0AQFo4kj')
+  )
+
+  check_parsed <- content(health_check,as='parsed')$checks
+
+  hc_names <- c()
+  hc_status <- c()
+  hc_latest <- c()
+
+  for (i in seq.int(1,length(check_parsed))){
+    hc_names[i] <- check_parsed[[i]][[1]] # json objects 1,7,9 are name, status, and last ping
+    hc_status[i] <- check_parsed[[i]][[7]]
+    hc_latest[i] <- check_parsed[[i]][[9]]
+  }
+
+  check_df <- data.frame(hc_names,hc_status,hc_latest)
+  check_df$hc_latest <- as.character(lubridate::as_datetime(check_df$hc_latest))
+
+  ## this is used for making a NOAA specific process table
+  if (context == 'NOAA'){
+    noaa_processes <- c('NOAA GEFS: stage 1','NOAA GEFS: stage 2','NOAA GEFS: stage 3')
+
+    checks_noaa <- check_df %>%
+      filter(hc_names %in% noaa_processes)
+
+    hc_table_noaa <- reactable(checks_noaa,
+                          columns = list(hc_names = colDef(name='Process Name'),
+                                         hc_status = colDef(name='Status'),
+                                         hc_latest = colDef(name = 'Updated (UTC)')),
+                          defaultPageSize = 10,
+                          filterable = TRUE,
+                          highlight = TRUE)
+    return(hc_table_noaa)
+
+    ## this is for making a table of ALL processes
+  } else if (conext == 'ALL'){
+
+  hc_table <- reactable(check_df,
+            columns = list(hc_names = colDef(name='Process Name'),
+                           hc_status = colDef(name='Status'),
+                           hc_latest = colDef(name = 'Updated (UTC)')),
+            defaultPageSize = 10,
+            filterable = TRUE,
+            highlight = TRUE)
+
+  return(hc_table)
+
+  } else{
+
+  print('Please specify conext for processes (ALL or NOAA)')
+
+    }
+
+}
